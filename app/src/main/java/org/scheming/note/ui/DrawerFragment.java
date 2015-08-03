f527b488d9e6e31d6217b56af11768976f2117cc
Scheming <1055062460@qq.com>
2015-08-03 18:49:16 +0800

    修改抽屉界面

---
+++ app/src/main/java/org/scheming/note/ui/DrawerFragment.java
@@ -0,0 +1,25 @@
+package org.scheming.note.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.scheming.note.R;
+
+public class DrawerFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_drawer, container, false);
+    }
+
+}
